4b7faa02b4ccf68869c875ff8b91a1cf0782be78
ignacio.sanchez@campusfp.es <ignacio.sanchez@campusfp.es>
2021-02-15 10:06:43 +0100

    master

---
+++ src/misrc/Main.java
@@ -0,0 +1,18 @@
+
+package misrc;
+public class Main {
+    public static void main(String[] args) {
+        
+        
+        public void sergiosuma() {
+            
+            
+        }
+        
+        public void nachoresta() {
+            
+            
+        }
+    }
+    
+}
